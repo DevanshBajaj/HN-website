9e79c0650eff9bbc3bf12513a5cf84fb6ff1c995
Devansh Bajaj <devansh21db.db@gmail.com>
2021-08-02 02:49:40 +0530

    fix: add fallback for max pages and news image

--- src/Components/NewsList.jsx
+++ src/Components/NewsList.jsx
@@ -1,7 +1,10 @@
 import { useState, useRef, useCallback } from "react";
+
 import { css } from "@emotion/react";
 import BarLoader from "react-spinners/BarLoader";
+
 import useNews from "../hooks/useNews";
+import Placeholder from "../assets/placeholder.jpg";
 
 const override = css`
 	display: block;
@@ -10,53 +13,71 @@ const override = css`
 `;
 
 const NewsList = () => {
-
 	const [pageNumber, setPageNumber] = useState(1);
+	const [pagesReached, setPagesReached] = useState(false);
+	const { news, hasMore, loading, error } = useNews(pageNumber);
 
-  const {
-    news,
-    hasMore,
-    loading,
-    error
-  } = useNews(pageNumber);
+	const observer = useRef();
 
-  const observer = useRef();
-  const lastNewsElementRef = useCallback(node => {
-    if (loading) return;
-    if (observer.current) observer.current.disconnect();
-    observer.current = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting && hasMore) {
-        console.log('intersecting');
-        setPageNumber(prevPageNumber => prevPageNumber + 1);
-      }
-    })
-    if (node) observer.current.observe(node);
-    console.log(node);
-  }, [loading, hasMore]);
+	const lastNewsElementRef = useCallback(
+		(node) => {
+			if (loading) return;
+			if (observer.current) observer.current.disconnect();
+			if (pageNumber === 10) {
+				setPagesReached(true);
+			} else {
+				observer.current = new IntersectionObserver((entries) => {
+					if (entries[0].isIntersecting && hasMore) {
+						console.log("intersecting");
+						setPageNumber((prevPageNumber) => prevPageNumber + 1);
+					}
+				});
+				if (node) observer.current.observe(node);
+				console.log(node);
+			}
+		},
+		[loading, hasMore]
+	);
 
 	return (
 		<div>
-      {news.map((newsItem , index) => {
-        if (news.length === index + 1) {
-          return (
-            <div key={newsItem.title}>
-              <h1>{newsItem.title}</h1>
-            </div>
-            )
-        } else {
-        return (
-        <div ref={lastNewsElementRef} key={newsItem.title}>
-          <h1>{newsItem.title}</h1>
-        </div>
-        )}}
-      )}
-      <BarLoader
-					color="#00a9e0"
-					loading={loading}
-					css={override}
-					size={150}
-				/>
-      <div>{error && 'error...'}</div>
+			{news.map((newsItem, index) => {
+				if (news.length === index) {
+					return (
+						<div key={index}>
+							<h1>{newsItem.title}</h1>
+						</div>
+					);
+				} else {
+					let url = newsItem.urlToImage;
+					if (url === null) {
+						url = Placeholder;
+					}
+					return (
+						<div ref={lastNewsElementRef} key={index}>
+							<img
+								style={{ height: "146px", width: "146px" }}
+								src={url}
+								alt="Image"
+							/>
+							<h1>{newsItem.title}</h1>
+						</div>
+					);
+				}
+			})}
+			{pagesReached ? (
+				<h1>Max Pages Reached</h1>
+			) : (
+				<div>
+					<BarLoader
+						color="#00a9e0"
+						loading={loading}
+						css={override}
+						size={150}
+					/>
+					<div>{error && "error..."}</div>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/hooks/useNews.jsx
+++ src/hooks/useNews.jsx
@@ -2,35 +2,43 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function useNews(pageNumber) {
-  const apiKey = import.meta.env.VITE_API_KEY;
+	const apiKey = import.meta.env.VITE_API_KEY;
 
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [news, setNews] = useState([]);
-  const [hasMore, setHasMore] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const [news, setNews] = useState([]);
+	const [hasMore, setHasMore] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    setError(false);
-    let cancel
-    axios({
-      method: 'GET',
-      url: 'https://newsapi.org/v2/top-headlines?country=in',
-      params: {pageSize: 10, page: pageNumber, apiKey: apiKey },
-      cancelToken: new axios.CancelToken(c => cancel = c)
-    }).then(response => {
-      setNews(prevNews => {
-        return [...new Set([...prevNews, ...response.data.articles])];
-      });
-      setHasMore(response.data.articles.length > 0);
-      setLoading(false)
-      console.log(response.data);
-    }).catch(error => {
-      if (axios.isCancel(error)) return
-      setError(true);
-    })
-    return () => cancel()
-  }, [pageNumber]);
+	useEffect(() => {
+		setLoading(true);
+		setError(false);
+		let cancel;
+		axios({
+			method: "GET",
+			url: "https://newsapi.org/v2/top-headlines?",
+			params: {
+				country: "in",
+				category: "technology",
+				pageSize: 10,
+				page: pageNumber,
+				apiKey: apiKey,
+			},
+			cancelToken: new axios.CancelToken((c) => (cancel = c)),
+		})
+			.then((response) => {
+				setNews((prevNews) => {
+					return [...new Set([...prevNews, ...response.data.articles])];
+				});
+				setHasMore(response.data.articles.length > 0);
+				setLoading(false);
+				console.log(response.data);
+			})
+			.catch((error) => {
+				if (axios.isCancel(error)) return;
+				setError(true);
+			});
+		return () => cancel();
+	}, [pageNumber]);
 
-  return {loading, error, news, hasMore};
+	return { loading, error, news, hasMore };
 }
