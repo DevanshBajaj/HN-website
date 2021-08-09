aaef3b55fd8ba04b1fabc4145d01bf0de8cca752
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-08-10 02:31:21 +0530

    style: style newslist cards

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,15 +1,17 @@
 import classes from './News.module.css';
+import Button from './UI/Button';
+import {ExternalLinkIcon} from '@heroicons/react/outline';
 
 const News = React.forwardRef((props, ref) => {
 	return (
 		<div ref={ref} key={props.id} className={classes.wrapper}>
 			<h1>{props.title}</h1>
 			<p>By: {props.user}</p>
+			<p>Published on &nbsp; <a href={props.domain} target={`${props.id}-domain`}>{props.UrlTitle}</a></p>
 			<p>
-				Link to the article: &nbsp;
-				<a href={props.url} target={props.id}>
-					{props.domain}
-				</a>
+				<Button href={props.url} target={props.id}>
+					Article <ExternalLinkIcon style={{height: '16px', width:'16px'}}/>
+				</Button>
 			</p>
 			<a
 				href={`https://news.ycombinator.com/item?id=${props.id}`}
