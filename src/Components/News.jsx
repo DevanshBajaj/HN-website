827ca227f46b81868649db4f274e2e297b31b61d
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-08-11 02:54:41 +0530

    style: style news list and make it responsive

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -5,22 +5,26 @@ import {ExternalLinkIcon} from '@heroicons/react/outline';
 const News = React.forwardRef((props, ref) => {
 	return (
 		<div ref={ref} key={props.id} className={classes.wrapper}>
-			<h1>{props.title}</h1>
-			<p>By: {props.user}</p>
-			<p>Published on &nbsp; <a href={props.domain} target={`${props.id}-domain`}>{props.UrlTitle}</a></p>
-			<p>
-				<Button href={props.url} target={props.id}>
-					Article <ExternalLinkIcon style={{height: '16px', width:'16px'}}/>
-				</Button>
-			</p>
+			<h1 className={classes.title}>{props.title}</h1>
+			<div className={classes.userWrapper}>
+				<p className={classes.user}>By: {props.user}</p> &nbsp;|&nbsp; 
+				<a href={props.domain} target={`${props.id}-domain`}>{props.UrlTitle}</a>
+			</div>
+			<div className={classes.dateWrapper}>
+				<p>{props.date}</p>
+				<p>Posted: {props.time_ago}</p>
+			</div>
 			<a
 				href={`https://news.ycombinator.com/item?id=${props.id}`}
 				target={`${props.id}-comments`}
 			>
 				{props.comments_count} Comments
 			</a>
-			<p>{props.date}</p>
-			<p>Posted: {props.time_ago}</p>
+			<p>
+				<Button href={props.url} target={props.id}>
+					Article <ExternalLinkIcon style={{height: '16px', width:'16px'}}/>
+				</Button>
+			</p>
 		</div>
 	);
 });
